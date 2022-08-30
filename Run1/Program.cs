62ff5a3570b61ef88fb7c89e84b24bb81df7cc26
Liudmila <vocrug-solnza@mail.ru>
2022-08-30 15:21:39 +0300

    Вывели 3 число

---
+++ Run1/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string? input = Console.ReadLine();
+
+char GetThirdNumber = string number;
+{
+    return[2];
+}
+
+char result = GetThirdNumber(input);
+
+Console.WriteLine(result);
