18432bb4cd93a45d8579a324de441b2922c9ca65
Liudmila <vocrug-solnza@mail.ru>
2022-09-02 20:29:23 +0300

    Возведение в степень одного числа в другое

---
+++ 4lessonC/Program.cs
@@ -0,0 +1,12 @@
+﻿double a;
+double b;
+double result;
+
+Console.WriteLine("Пожалуйста, введите число,которое вы хотите возвести в степень: ");
+a = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Спасибо,теперь введите степень, в которую хотите возвести число "+a+"");
+b = Convert.ToDouble(Console.ReadLine());
+result = Math.Pow(a, b);
+
+Console.WriteLine("Получилось! "+a+" в степени "+b+" равно "+result+"!" );
