831327478f341b71f5a898b60d5480377f21f291
Liudmila <vocrug-solnza@mail.ru>
2022-09-02 20:24:38 +0300

    Сложение цифр числа

---
+++ Lesson4,2/Program.cs
@@ -0,0 +1,26 @@
+﻿//Программа, которая принимает на вход число и выдаёт сумму цифр в числе:452->11,82->10,9012->12
+Console.WriteLine("Введите число: ");
+
+bool isNumber=int.TryParse(Console.ReadLine(), out int number);
+
+if(isNumber==false)
+{
+    Console.WriteLine("Некорректный ввод");
+    return;
+}
+int result = GetSumNumber(number);
+Console.WriteLine("Сумма цифр в числе равна: "+result+"");
+
+int GetSumNumber(int number)
+{
+    int sum = 0;
+    while (number!=0)
+    {
+        sum = sum + number%10;
+        number=number/10;
+    }
+    return sum;
+}
+
+
+
