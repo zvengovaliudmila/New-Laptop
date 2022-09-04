df55ca507e15ea61195e845ed4767bd53a79eb32
Liudmila <vocrug-solnza@mail.ru>
2022-09-04 14:11:46 +0300

    Ввели массив с консоли и вывели его в консоль

---
+++ Массив 8 элементов,/Program.cs
@@ -0,0 +1,41 @@
+﻿#nullable disable
+
+
+   Console.WriteLine("Введите длину массива:");
+   bool isNumber = int.TryParse(Console.ReadLine(), out int length);
+
+   if(!isNumber || length <= 0)
+   {
+       Console.WriteLine("Неправильный ввод");
+       throw new Exception("Неправильный ввод!");
+   }
+
+   FillArray(length);
+   
+   int[] FillArray(int length)
+   {
+      int[]array = new int [length];
+      string input = Console.ReadLine();
+      string[] inputArray = input.Split(",");
+
+      for( int i = 0; i < inputArray.Length; i++)
+      {
+         Console.Write($"{inputArray[i]}");
+      }
+
+      return array;
+   }
+   
+
+
+
+
+
+
+
+
+
+
+   
+
+
