fe5fc6175983a53f3999ec86970d8920c2586c15
Liudmila <vocrug-solnza@mail.ru>
2022-09-08 17:08:25 +0300

    Перевернули массив

---
+++ Перевернули 1мерный масив/Program.cs
@@ -0,0 +1,27 @@
+﻿//Перевепнуть одномерный массив.
+
+int[] array = new int[] {1,2,3,4,5};
+PrintArray(array);
+int [] reverseArray = ReverseArray(array);
+Console.WriteLine();
+PrintArray(reverseArray);
+
+void PrintArray (int[] array)
+{
+     for(int i = 0; i < array.Length; i++)
+     {
+     Console.Write($"{array[i]}");
+     }
+}
+
+int[] ReverseArray (int[] array)
+{
+
+    int [] reverseArray = new int[array.Length];
+    for ( int i = 0; i < array.Length; i++)
+    {
+        reverseArray[i] = array[array.Length - 1 - i];
+    }
+
+    return reverseArray;
+}
