b729080da73a8be77dd8b00a0fd038eef353a7ab
Liudmila <vocrug-solnza@mail.ru>
2022-09-08 22:24:15 +0300

    Числа Фибоначчи без рекурсии

---
+++ Числа Фибоначчи без рекурсии/Program.cs
@@ -0,0 +1,54 @@
+﻿//Каждое последующее число - это сумма двух предыдущих.
+
+
+
+
+int[] GetFib(int N)
+{
+    int[] result = new int[N];
+    result[0] = 0;
+    if(N < 2)
+    {
+    return result;
+
+    }
+    result[1] = 1;
+    for(int i = 2; i < N; i++ )
+    {
+        result[i] = result[i-1] +  result[i-2];
+
+    }
+    return result;
+}
+
+void PrintArray (int[]array)
+{
+     if ( array.Length== 0)
+     {
+         Console.WriteLine( "Что-то пошло не так");
+         return;
+    }
+    Console.Write("[");
+
+     for(int i = 0; i < array.Length - 1; i++)
+     {
+          Console.Write($"{array[i]},");
+     }
+     Console.Write(array[array.Length -1]);
+     Console.Write("]");
+}
+
+
+
+Console.WriteLine("Enter length: ");
+bool isNumber= int.TryParse(Console.ReadLine(), out int length);
+
+if(!isNumber || length <= 0)
+{
+   Console.WriteLine("Invalid number");
+   return;
+}
+
+
+int [] result = GetFib(length);
+PrintArray(result);
