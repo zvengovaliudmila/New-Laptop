783410ece2f43dce8b1b081b9fdf811c3932158a
Liudmila <vocrug-solnza@mail.ru>
2022-09-08 19:52:33 +0300

    Преобразование числа

---
+++ Десятичное в двоичное,/Program.cs
@@ -0,0 +1,8 @@
+﻿#nullable disable
+
+//Преобразование десятичного числа в двоичное.45 -> 101101, 3 -> 11, 2->10
+Console.WriteLine("Введите десятичное число:");
+int i = Convert.ToInt32(Console.ReadLine());
+string s = Convert.ToString(i, 2);
+Console.WriteLine(" В двоичной системе исчисления это:" +s);
+
