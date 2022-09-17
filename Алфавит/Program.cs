222d06f53ae12130c31a6365b01abfd14e1c7fcc
Liudmila <vocrug-solnza@mail.ru>
2022-09-17 14:59:21 +0300

    Из заданного количества букв получаем двубуквенные слова

---
+++ Алфавит/Program.cs
@@ -0,0 +1,18 @@
+﻿
+// Находим из заданного количества букв двубуквенные слова
+
+int n = 1;
+
+void FindWords(string alphabet, char [] word, int length = 0)
+{
+   if ( length == word.Length)
+   {
+      Console.WriteLine($"{n++} {new String(word)}"); return;
+   }
+   for ( int i = 0; i < alphabet.Length; i++)
+   {
+       word[length] = alphabet[i];
+       FindWords(alphabet, word, length +1);
+   }
+}
+FindWords("аисв", new char[2]);
