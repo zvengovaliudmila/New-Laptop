7281cac1787eed2d9f04735c0207cb57754db609
Liudmila <vocrug-solnza@mail.ru>
2022-09-18 18:38:45 +0300

    Простой пример рекурсии - натуральные числа от m до n

---
+++ 64задача9LC#/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Задайте значения M и N. Напишите программу, которая выведет все натуральные числа 
+в промежутке от M до N.
+
+M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+
+M = 4; N = 8. -> ""4, 6, 7, 8""*/
+
+
+void Recursive (int m, int n, int i = 0)
+{
+     if(i==n)
+     {
+        return;
+     }
+
+    Console.Write($"{m}, ");
+    Console.WriteLine();
+    Recursive (++m,n, ++i);
+}
+
+
+Recursive(4, 8);
+
