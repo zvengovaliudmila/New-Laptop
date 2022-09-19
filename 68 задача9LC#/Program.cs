000fdd9e79ae013f3c26ef370c00b57dc17e8795
Liudmila <vocrug-solnza@mail.ru>
2022-09-19 22:38:13 +0300

    Функция Аккермана с помощью рекурсии

---
+++ 68 задача9LC#/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+Даны два неотрицательных числа m и n.
+
+m = 2, n = 3 -> A(m,n) = 9*/
+
+
+int AkRec( int m, int n)
+{
+  if( m==0)
+     return n + 1;
+  else
+    if ((m!=0) && (n == 0))
+     return AkRec(m-1, 1);
+    else
+     return AkRec(m-1, AkRec(m, n - 1));
+
+}
+Console.WriteLine(AkRec(2, 3));
