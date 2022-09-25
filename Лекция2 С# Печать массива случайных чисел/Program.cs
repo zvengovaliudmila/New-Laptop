17776e2762f85a890e0e7c49d0a8b82e892ec5a7
Liudmila <vocrug-solnza@mail.ru>
2022-09-25 16:23:31 +0300

    Печать массива

---
+++ Лекция2 С# Печать массива случайных чисел/Program.cs
@@ -0,0 +1,29 @@
+﻿// 
+
+int[] array = new int[10];
+
+
+
+void FillArray ( int[]collection)
+{
+       int length = collection.Length;
+       int index = 0;
+       while( index < length)
+       {
+             collection[index] = new Random().Next(1,10);
+             index++;
+       }
+}
+void PrintArray(int[] col)
+{
+       int count = col.Length;         // количество элементов.
+       int position = 0;               // вместо index
+       while(position < count)
+       {
+          Console.WriteLine(col[position]);        // значение текущего элемента
+          position++;                              // увеличиваем значение текущей позиции
+       }
+}
+
+FillArray(array);
+PrintArray(array);
